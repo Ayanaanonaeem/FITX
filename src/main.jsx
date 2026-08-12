f1cb3e39b09567b92144d485a870dda53f733391
Ayan Naeem <ayanaano5@gmail.com>
2026-08-12 22:33:28 +0500

    few changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import { ThemeProvider } from './Context/ThemeContext'
 import { ThemeProvider } from './Context/ThemeContext'
 import App from './App'
 import './index.css'
